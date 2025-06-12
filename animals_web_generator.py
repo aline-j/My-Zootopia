87ae139348b2e0acab2bd855071d4b249a09a7a9
Aline Janke <alinejanke@Mac.fritz.box>
2025-06-12 09:44:45 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,7 +2,9 @@ import json
 
 
 def load_data(file_path):
-    """ Loads a JSON file """
+    """
+    Loads a JSON file
+    """
     with open(file_path, "r") as handle:
         return json.load(handle)
 
@@ -11,18 +13,32 @@ animals_data = load_data('My-Zootopia/animals_data.json')
 
 
 def get_animal_data():
-    for animal in animals_data:
-        name = animal['name']
-        diet = animal['characteristics']['diet']
-        location = animal['locations'][0]
-
-        if 'type' in animal['characteristics']:
-            animal_type = animal['characteristics']['type']
-            print(f'Name: {name}')
-            print(f'Diet: {diet}')
-            print(f'Location: {location}')
-            print(f'Type: {animal_type}')
+    """
+    Formats animal data into a plain text block,
+    with each animal's info on separate lines.
+    """
+    output = ''  # define an empty string
+    for animal_data in animals_data:
+        # append information to each string
+        output += f"Name: {animal_data['name']}\n"
+        output += f"Diet: {animal_data['characteristics']['diet']}\n"
+        output += f"Location: {animal_data['locations'][0]}\n"
+        if 'type' in animal_data['characteristics']:
+            output += f"Type: {animal_data['characteristics']['type']}\n"
         else:
-            print(f'Name: {name}')
-            print(f'Diet: {diet}')
-            print(f'Location: {location}')
+            continue
+    return output
+
+
+output = get_animal_data()
+
+# Read the HTML template
+with open('My-Zootopia/animals_template.html', 'r') as htmlfile:
+    template = htmlfile.read()
+
+# Replace placeholder with animal info
+final_html = template.replace('__REPLACE_ANIMALS_INFO__', output)
+
+# Write the final HTML
+with open('My-Zootopia/animals.html', 'w') as newhtmlfile:
+    newhtmlfile.write(final_html)
